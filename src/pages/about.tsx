6f5d2e71e5cf4a448dc013adf109c89225d9a773
marcioandrade <marcioandrade.dev@gmail.com>
2021-09-10 10:21:39 -0300

    entendendo os metodos de getStatic/getServerSide do nextJS e escrevendo queries

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,22 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
 import AboutTemplate from 'templates/About'
 
-const About = () => <AboutTemplate />
+export default function AboutPage() {
+  return <AboutTemplate />
+}
 
-export default About
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {},
+  }
+}
+
+// getStaticPaths => serve para gerar as urls em build time /about /trip/petropolis
+// getStaticProps => Serve para buscar dados da página (props) - build time - estático
+// getServerSideProps => Serve para buscar dados da página (props) - Runtime - rota requisição (Bundle fica no server)
+// getInitialProps = > Serve para buscar dados da página (props) - Runtime - rota requisição (Bundle também vem para o client) - hydrate (Pouca gente usa e o NextJs não recomenda)
