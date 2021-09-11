053ae7fb5fb8239a532017f2d1f56b63844d773a
marcioandrade <marcioandrade.dev@gmail.com>
2021-09-11 12:52:38 -0300

    configurando SEO para as paginas com Next-SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'pt_BR',
+    url: 'https://mytrips.com',
+    site_name: 'My Trips',
+  },
+  twitter: {
+    handle: '@marciooxd',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+}
